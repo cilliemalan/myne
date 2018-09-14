3a3668bec31d0357556d8a7b933bd4f6b26f20d0
Cillié Malan <cilliemalan@gmail.com>
2018-09-14 13:15:57 +0200

    Added base projects

---
+++ src/device/main.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+
+int main()
+{
+    printf("hello from MyneDevice!\n");
+    return 0;
+}
---
+++ src/server/main.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+
+int main()
+{
+    printf("hello from MyneServer!\n");
+    return 0;
+}
