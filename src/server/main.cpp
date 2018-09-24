fed94a877793e03c59397de2fb4b441ec985b27f
Cillié Malan <cilliemalan@gmail.com>
2018-09-24 17:39:46 +0200

    Added line echo class

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,13 +1,48 @@
 #include "pch.hpp"
 #include "Listener.hpp"
 
+
+class LineEcho : public SocketStream
+{
+public:
+	LineEcho(int fd) : SocketStream(fd) {}
+
+	virtual void receive(void* data, int length)
+	{
+		auto p = static_cast<char*>(data);
+		auto bufflen = _lineBuffer.size();
+		_lineBuffer.insert(_lineBuffer.end(), p, p + length);
+
+		write_lines(bufflen);
+	}
+private:
+
+	void write_lines(size_t start_scan_at = 0)
+	{
+		auto lbs = _lineBuffer.size();
+		if (lbs > start_scan_at)
+		{
+			size_t len = start_scan_at + 1;
+			for (auto pc = _lineBuffer.begin() + start_scan_at; pc != _lineBuffer.end(); pc++, len++)
+			{
+				if (*pc == '\n')
+				{
+					buffered_send(&_lineBuffer[0], len);
+					memcpy(&_lineBuffer[0], &_lineBuffer[len], lbs - len);
+					_lineBuffer.resize(lbs - len);
+					write_lines();
+					break;
+				}
+			}
+		}
+	}
+
+	std::vector<char> _lineBuffer;
+};
+
 int main(int argc, char *argv[])
 {
-	std::function<SocketStream*(int)> streamFactory = [](int fd) { return new FunctionalSocketStream(fd, 
-		[](int fd, void* data, int length, std::function<int(void* data, int length)> write)
-	{
-		write(data, length);
-	}); };
+	std::function<SocketStream*(int)> streamFactory = [](int fd) { return new LineEcho(fd); };
 
 	printf("starting listener\n");
 	Listener l(nullptr, 80, streamFactory);
